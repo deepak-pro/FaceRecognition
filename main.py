75cb58fb7c1e1ef463cdb7505fa34c0f2ecd0ff2
Deepak JOSHI <joshideepak4598@gmail.com>
2020-01-03 14:53:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import face_recognition
+
+billGatesPicture = face_recognition.load_image_file("bill.jpg")
+billFaceEncoding = face_recognition.face_encodings(billGatesPicture)[0]
+
+unknownPicture = face_recognition.load_image_file("unknown.jpg")
+unknownFaceEncoding = face_recognition.face_encodings(unknownPicture)[0]
+
+results = face_recognition.compare_faces([billFaceEncoding],unknownFaceEncoding)
+
+if results[0] == True:
+    print("Unknown Picture is of Bill Gates")
+else:
+    print("Unknown Picture is not of Bill Gates")
+
